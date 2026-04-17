6595ae213aa73b4c5f1458b1d92c3f39bd97ad98
Luciaperazzolo <luciaperazzolo22@gmail.com>
2026-04-17 17:22:50 -0300

    Se agregó la lupa en el la barra del buscador; Se agregó la flecha en el link de volver (para esto use en ambos la libreria FontAwesome); modifique el archivo header de partials y el header de css; Agregue los enlaces de navegación del carrito al inicio y del botón de pagar al checkout.

--- Mi-Ecommerce-Sprint1/app.js
+++ Mi-Ecommerce-Sprint1/app.js
@@ -9,18 +9,15 @@ app.use(express.static(__dirname +'/assets'));
 //--- DEFINICIÓN DE RUTAS (USER STORY #2) ---
 app.get ('/index', (req, res) => {res.render("pages/index");
 });
-
 app.get ('/cart', (req, res) => {res.render ('pages/cart');
 });
-app.get('/product', (req, res) => {
-    res.render("pages/product");
+app.get('/product', (req, res) => {res.render("pages/product");
 });
-
-
-//Acá abajo deberiamos definir las rutas que faltan.
+app.get('/checkout', (rep, res) => {res.render("pages/checkout")
+});
 
 //--- PUESTA EN MARCHA ---
 app.listen(3000, () => { //Encender servidor
-    console.log("Servidor corriendo en http://localhost:3000"); //mje en terminal: "Servidor funcionando en: http://localhost:3000"
-});
+    console.log("Servidor corriendo en http://localhost:3000");
+});//mje en terminal: "Servidor funcionando en: http://localhost:3000"
 
