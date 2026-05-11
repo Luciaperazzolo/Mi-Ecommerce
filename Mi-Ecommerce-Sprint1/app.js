cf2e2d0bc06806035e4ce358270a637bc541a73c
LuciaPerazzolo <luciaperazzolo22@gmail.com>
2026-05-11 17:53:06 -0300

    Mejoras estéticas en el checkout y ajustes de funcionalidad

--- Mi-Ecommerce-Sprint1/app.js
+++ Mi-Ecommerce-Sprint1/app.js
@@ -1,21 +1,26 @@
 //--- IMPORTACIÓN DE MÓDULOS ---
 const express = require ('express'); //Traigo la Heramienta
+const session = require('express-session'); //Herramienta para usar sesiones
+const productRoutes = require('./routes/productRoute'); //Importar las rutas relacionadas con productos.
 const app = express (); //La pongo en función
 
 //--- CONFIGURACIÓN DEL SERVIDOR ---
-app.set ('view engine', 'ejs');
-app.use(express.static(__dirname +'/assets'));
+app.set ('view engine', 'ejs'); //Configuro el motor de plantillas para que use EJS, lo que me permite generar HTML dinámico a partir de mis vistas.
+app.use(express.static(__dirname +'/assets')); //Configuro la carpeta de archivos estáticos, lo que me permite servir CSS, imágenes y otros recursos directamente desde esa carpeta sin necesidad de rutas específicas para cada uno.
+
+//--- ACTIVO LA HERRAMIENTA DE SESIONES ---
+app.use(session({ //Activando el sistema de sesiones.
+    secret: 'claveUltraSecreta_carrito', //Contraseña interna que usa el servidor para firmar la sesión y evitar que la manipulen.
+    resave: false, //No guarda la sesión si no cambio algo de página en página.
+    saveUninitialized: false //Evita que se guarden sesiones vacias.
+}));
 
 //--- DEFINICIÓN DE RUTAS (USER STORY #2) ---
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
 
-app.get('/', (req, res) => {res.render('pages/index');
-});
-app.get ('/cart', (req, res) => {res.render ('pages/cart');
-});
-app.get('/product', (req, res) => {res.render("pages/product");
-});
-app.get('/checkout', (rep, res) => {res.render("pages/checkout")
-});
+app.use('/product', productRoutes); //Usar las rutas relacionadas con productos cuando el usuario acceda a /product
 
 //--- PUESTA EN MARCHA ---
 app.listen(3000, () => { //Encender servidor
