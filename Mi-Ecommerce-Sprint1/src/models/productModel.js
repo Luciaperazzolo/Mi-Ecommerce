f1f328312055c16ac1c7c6c94c3deac7ea88373a
beree44 <bernice.gongora.5@gmail.com>
2026-05-12 01:49:01 -0300

    Update productModel.js
    Refactored product model to read products from a JSON file instead of a hardcoded array. Added methods to find all products and find a product by ID.

--- Mi-Ecommerce-Sprint1/src/models/productModel.js
+++ Mi-Ecommerce-Sprint1/src/models/productModel.js
@@ -1,32 +1,23 @@
-const products = [
-  {
-    id: 1,
-    name: "Jack Daniel's Tennessee Honey",
-    price: 9900,
-    image: "/imagenes-productos/Jack Daniels.jpg"
-  },
-  {
-    id: 2,
-    name: "Combo Hamburguesa",
-    price: 5000,
-    image: "/imagenes-productos/combo-hamburguesa.png"
-  },
-  {
-    id: 3,
-    name: "Coca Cola lata 220ml x8",
-    price: 1520,
-    image: "/imagenes-productos/lata-cocacola.png"
-  }
-];
+const fs = require('fs');
+const path = require('path');
 
-//Funciones para usar los datos.
-exports.getAllProducts = () => products; //Retorna el array completo de productos.
+const productModel = {
+    // 1. Buscamos la ruta del archivo JSON
+  
+    jsonPath: path.join(__dirname, '../../data/products.json'),
 
-//Recibe el identificador único del producto que quieres buscar (por ejemplo, un número o un string).
-//Es un método de los arrays en JavaScript. Recorre la lista de productos uno por uno.
-//p representa cada producto individual mientras el método busca.
-//La función se detiene y devuelve el primer producto donde el ID del objeto (p.id) coincida con el ID que se le paso a la función.
-//La variable products es la que recorres con el <% cart.forEach(...) %>.
-exports.getProductById = (id) => {
-  return products.find(p => p.id == id);
+    // 2. Función para leer todos los productos
+    findAll: function() {
+        const jsonContent = fs.readFileSync(this.jsonPath, 'utf-8');
+        return JSON.parse(jsonContent);
+    },
+
+    // 3. Función para buscar un producto por su ID
+    findById: function(id) {
+        const allProducts = this.findAll();
+        const productFound = allProducts.find(p => p.id == id);
+        return productFound;
+    }
 };
+
+module.exports = productModel;
