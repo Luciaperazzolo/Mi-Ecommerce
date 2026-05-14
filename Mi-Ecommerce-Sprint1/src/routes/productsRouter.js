da8a51257c3c2ff22aa9a6c4eb8f5a4c9a42fe6b
beree44 <bernice.gongora.5@gmail.com>
2026-05-14 01:23:36 -0300

    Add category route to productsRouter

--- Mi-Ecommerce-Sprint1/src/routes/productsRouter.js
+++ Mi-Ecommerce-Sprint1/src/routes/productsRouter.js
@@ -4,5 +4,5 @@ const productsController = require('../controllers/productsController');
 
 // Definimos la ruta para el detalle. El :id es un parámetro dinámico
 router.get('/:id', productsController.detalle);
-
+router.get('/category/:name', productsController.categoria);
 module.exports = router;
