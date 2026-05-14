3047e0727070a8456e3e9c6175ea83b09a145613
beree44 <bernice.gongora.5@gmail.com>
2026-05-14 01:20:13 -0300

    Add category filtering method to productsController

--- Mi-Ecommerce-Sprint1/src/controllers/productsController.js
+++ Mi-Ecommerce-Sprint1/src/controllers/productsController.js
@@ -1,27 +1,37 @@
-const productModel = require('../models/productModel'); // Verificá que esta ruta a tu modelo sea correcta
+const productModel = require('../models/productModel'); 
 
 const productsController = {
-
     detalle: (req, res) => {
         const id = req.params.id;
         const allProducts = productModel.findAll();
         const product = productModel.findById(id);
 
         if (product) {
-            // 1. Filtramos: que no sea el mismo Y que sea de la misma categoría
             let related = allProducts.filter(p => p.id != product.id && p.category == product.category);
-            
-            // 2. Mezclamos el array para el azar
             related.sort(() => Math.random() - 0.5);
-            
-            // 3. Cortamos para que solo pasen 4 a la vista
             const relatedProducts = related.slice(0, 4);
 
             res.render('pages/products', { product, relatedProducts });
         } else {
-            // 4. Si el ID no existe (ej: 999), muestra este mensaje
-            res.send("<h1>Producto no disponible</h1><a href='/'>Volver al inicio</a>");
+            res.status(404).render('pages/404');
         }
+    },
+
+    // MÉTODO PARA FILTRAR POR CATEGORÍA
+    categoria: (req, res) => {
+        const nombreCategoria = req.params.name; 
+        const todosLosProductos = productModel.findAll();
+
+       
+        const filtrados = todosLosProductos.filter(p => 
+            p.category.toLowerCase() === nombreCategoria.toLowerCase()
+        );
+
+     
+        res.render('pages/productsCategory', { 
+            products: filtrados, 
+            categoryName: nombreCategoria 
+        });
     }
 };
 
