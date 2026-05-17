4387d9abbeccf57f2b71337216bff46d9030469c
beree44 <bernice.gongora.5@gmail.com>
2026-05-17 02:58:20 -0300

    Add catalog route and improve route comments
    Added catalog route and updated comments for clarity.

--- Mi-Ecommerce-Sprint1/src/routes/productsRouter.js
+++ Mi-Ecommerce-Sprint1/src/routes/productsRouter.js
@@ -1,8 +1,16 @@
-const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 
-// Definimos la ruta para el detalle. El :id es un parámetro dinámico
-router.get('/:id', productsController.detalle);
+//  Catálogo con todos los productos juntos
+router.get('/', productsController.catalogo);
+
+//Filtra al tocar la barra oscura
+// URL: localhost:3000/products/category/Alimentos
 router.get('/category/:name', productsController.categoria);
+
+//  Muestra un solo producto por su ID
+//
+// URL: localhost:3000/products/1
+router.get('/:id', productsController.detalle);
+
 module.exports = router;
