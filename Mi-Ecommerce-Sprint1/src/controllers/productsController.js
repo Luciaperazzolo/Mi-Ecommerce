2a3131b6b63dcdff8e95dab25ad8289fe0fd59a0
beree44 <bernice.gongora.5@gmail.com>
2026-05-17 02:56:04 -0300

    Update product controller methods and rendering views

--- Mi-Ecommerce-Sprint1/src/controllers/productsController.js
+++ Mi-Ecommerce-Sprint1/src/controllers/productsController.js
@@ -1,6 +1,15 @@
 const productModel = require('../models/productModel'); 
 
 const productsController = {
+    //  Muestra  todos los productos juntos '
+    catalogo: (req, res) => {
+        const todosLosProductos = productModel.findAll();
+        res.render('pages/products', { 
+            products: todosLosProductos 
+        });
+    },
+
+    //  Muestra un solo producto por su ID)
     detalle: (req, res) => {
         const id = req.params.id;
         const allProducts = productModel.findAll();
@@ -11,23 +20,21 @@ const productsController = {
             related.sort(() => Math.random() - 0.5);
             const relatedProducts = related.slice(0, 4);
 
-            res.render('pages/products', { product, relatedProducts });
+            res.render('pages/productDetail', { product, relatedProducts });
         } else {
             res.status(404).render('pages/404');
         }
     },
 
-    // MÉTODO PARA FILTRAR POR CATEGORÍA
+    // Filtra por sección (Alimentos, Bebidas)
     categoria: (req, res) => {
         const nombreCategoria = req.params.name; 
         const todosLosProductos = productModel.findAll();
 
-       
         const filtrados = todosLosProductos.filter(p => 
             p.category.toLowerCase() === nombreCategoria.toLowerCase()
         );
 
-     
         res.render('pages/productsCategory', { 
             products: filtrados, 
             categoryName: nombreCategoria 
